1bc996937e685caeac459ca0de6b80f5d2637f34
Alexandre Lages <alexandrelages64@gmail.com>
2018-03-22 11:28:37 -0300

    Alteração das entidades e mapeamento com hibernate

--- src/main/java/br/com/engenharia/model/Publicacao.java
+++ src/main/java/br/com/engenharia/model/Publicacao.java
@@ -2,21 +2,69 @@ package br.com.engenharia.model;
 
 import java.util.Date;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+@Entity
 public class Publicacao {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	
+	private String titulo;
 	private String conteudo;
+	
+	@Temporal(value = TemporalType.TIMESTAMP)
 	private Date dataDaPostagem;
 	
+	@ManyToOne
+	@JoinColumn(name = "id_usuario")
+	private Usuario usuario;
+	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
 	public Date getDataDaPostagem() {
 		return dataDaPostagem;
 	}
+	
 	public void setDataDaPostagem(Date dataDaPostagem) {
 		this.dataDaPostagem = dataDaPostagem;
 	}
+	
 	public String getConteudo() {
 		return conteudo;
 	}
+	
 	public void setConteudo(String conteudo) {
 		this.conteudo = conteudo;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
 	}
 }
--- src/main/java/br/com/engenharia/model/Usuario.java
+++ src/main/java/br/com/engenharia/model/Usuario.java
@@ -2,44 +2,77 @@ package br.com.engenharia.model;
 
 import java.util.List;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+
+@Entity
 public class Usuario {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
 	
 	private String nome;
 	private String email;
 	private String senha;
 	
+	@ManyToMany(cascade = CascadeType.MERGE, fetch = FetchType.LAZY)
 	private List<Usuario> amigos;
+	
+	@OneToMany(mappedBy = "usuario", targetEntity = Publicacao.class, fetch = FetchType.LAZY, cascade = CascadeType.ALL)
 	private List<Publicacao> publicacoes;
 	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
 	public String getNome() {
 		return nome;
 	}
+	
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+	
 	public String getEmail() {
 		return email;
 	}
+	
 	public void setEmail(String email) {
 		this.email = email;
 	}
+	
 	public String getSenha() {
 		return senha;
 	}
+	
 	public void setSenha(String senha) {
 		this.senha = senha;
 	}
+
 	public List<Usuario> getAmigos() {
 		return amigos;
 	}
+
 	public void setAmigos(List<Usuario> amigos) {
 		this.amigos = amigos;
 	}
+
 	public List<Publicacao> getPublicacoes() {
 		return publicacoes;
 	}
+
 	public void setPublicacoes(List<Publicacao> publicacoes) {
 		this.publicacoes = publicacoes;
 	}
-
 }
